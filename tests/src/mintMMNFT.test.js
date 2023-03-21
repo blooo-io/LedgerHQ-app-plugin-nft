5ff1d62b1daccfb8c96b70bffb608696b3cea985
Z4karia <92750334+Z4karia@users.noreply.github.com>
2023-03-21 13:31:37 +0000

    test: renamed test files

--- tests/src/mintMMNFT.test.js
+++ tests/src/mintMMNFT.test.js
@@ -2,8 +2,8 @@ import { processTest, populateTransaction } from "./test.fixture";
 
 const contractName = "MultiMintContractNFT";
 
-const testLabel = "Mint"; // <= Name of the test
-const testDirSuffix = "mint"; // <= directory to compare device snapshots to
+const testLabel = "MintMMNFT"; // <= Name of the test
+const testDirSuffix = "mint_mmnft"; // <= directory to compare device snapshots to
 const signedPlugin = false;
 const testNetwork = "ethereum_goerli";
 
--- tests/src/mintSMM721.test.js
+++ tests/src/mintSMM721.test.js
@@ -2,8 +2,8 @@ import { processTest, populateTransaction } from "./test.fixture";
 
 const contractName = "StableMultiMintERC721";
 
-const testLabel = "mintV2"; // <= Name of the test
-const testDirSuffix = "mint_v2"; // <= directory to compare device snapshots to
+const testLabel = "mintSMM721"; // <= Name of the test
+const testDirSuffix = "mint_smm721"; // <= directory to compare device snapshots to
 const signedPlugin = false;
 const testNetwork = "ethereum_goerli";
 
